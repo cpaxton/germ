a2ac1c85be3ca669d7afe72130a179a5f692a515
Christopher Paxton <cpaxton3@jhu.edu>
2014-10-24 11:38:08 -0400

    adding files from predicator for producing messages

---
+++ germ_predicator/src/germ_predicator/moveit_predicator_node.cpp
@@ -0,0 +1,27 @@
+// ROS
+#include <ros/ros.h>
+
+// for debugging
+#include <iostream>
+
+// predicator stuff
+#include "predicator.h"
+
+int main(int argc, char **argv) {
+
+  ros::init(argc, argv, "moveit_predicator_node");
+
+  germ_predicator::PredicateContext pc(true);
+
+  // define spin rate
+  ros::Rate rate(30);
+
+  // start main loop
+  while(ros::ok()) {
+    ros::spinOnce();
+    pc.tick();
+    rate.sleep();
+  }
+
+  pc.cleanup();
+}
